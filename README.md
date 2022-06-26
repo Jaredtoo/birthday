aa827a24fa0222dc1c88774dfbe11d1769413a4f
jared <jaredtoo95@gmail.com>
2022-06-26 13:57:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+## BORN NAME
+### AN APPLICATION THAT DERIVES THE AFRICAN WAY OF NAMING CHILDREN USING DAY; BORN NAME, DECEMBER 2021
+#### By Jared Too
+## Description
+Sometimes people may forget the day of the week that they were born. But South Africa has a cultural way of naming their children using the day that they were born. Every day of the week has a different name for male and female children. This application takes user details and uses the details to calculate the day of the week and displays the day of the week that he/she was born and also their Born names. 
+## Setup/Installation Requirements
+* Open terminal
+* Change directory to the location you want to copy the project to.
+* Clone the repo {git clone https://jaredtoo.github.io/birthday/}
+* Now you have the project in your local machine
+## Behaviour
+
+* Check if it's male born on Sunday the outcome will be Kwasi.
+* Check if it's male born on Monday the outcome will be Kojo.
+* Check if it's male born on Tuesday the outcome will be Kwabena.
+* Check if it's male born on Wednesday the outcome will be Kwaku.
+* Check if it's male born on Thursday the outcome will be Yaw.
+* Check if it's male born on Friday the outcome will be Kofi.
+* Check if it's male born on Suturday the outcome will be Kwame.
+  
+* Check if it's female born on Sunday the outcome will be Akosua.
+* Check if it's female born on Monday the outcome will be Adwoa.
+* Check if it's female born on Tuesday the outcome will be Abenaa.
+* Check if it's female born on Wednesday the outcome will be Akua.
+* Check if it's female born on Thursday the outcome will be Yaa.
+* Check if it's female born on Friday the outcome will be Afua.
+* Check if it's female born on Suturday the outcome will be Ama.
+### Live Site
+View [live](https://jaredtoo.github.io/birthday/)
+## Technologies Used
+* HTML
+* Javascript
+## Support and contact details
+Kindly let us know your feedbacks and concerns and you can also ask questions through our email address jaredtoo95@gmail.com
+### License
+This is under the [MIT](LICENSE) license
+Copyright (c) 2021 Jared Too
---
+++ js/script.js
@@ -0,0 +1,138 @@
+
+function calculate() {
+
+    var fname = document.getElementById("firstname").value;
+
+
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    if (century() == 20) {
+
+    }
+    var dayOfTheWeek = Math.floor((year() + month() + century() + day() - leapYearCode()) % 7);
+    if (fname == "" && gender() == 0 && day().day == null) {
+        alert("Provide your name, gender and day of birth.")
+    } else if (fname == "" && gender() == 0) {
+        alert("Provide your name and gender.");
+    } else if (fname == "" && day().day == null) {
+        alert("Provide your name and day of birth.")
+    } else if (gender() == 0 && day().day == null) {
+        alert("Select your gender and provide your day of birth.");
+    } else if (fname == "") {
+        alert("Provide your preferred name")
+    } else if (gender() == 0) {
+        alert("Select your gender.")
+    } else if (day() == 0) {
+        alert("Provide your day of birth")
+    } else {
+        if (day() <= 0 || day() > 31) {
+            alert("Invalid day!");
+        } else {
+            if (gender() == 1) {
+                if (dayOfTheWeek == 0) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[0] + " because you were born a male on a Sunday.";
+                } else if (dayOfTheWeek == 1) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[1] + " because you were born a male on a Monday.";
+                } else if (dayOfTheWeek == 2) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[2] + " because you were born a male on a Tuesday.";
+                } else if (dayOfTheWeek == 3) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[3] + " because you were born a male on a Wednesday.";
+                } else if (dayOfTheWeek == 4) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[4] + " because you were born a male on a Thursday.";
+                } else if (dayOfTheWeek == 5) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[5] + " because you were born a male on a Friday.";
+                } else {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + male[6] + " because you were born a male on a Saturday.";
+                }
+            } else if (gender() == 2) {
+                if (dayOfTheWeek == 0) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[0] + " because you were born a female on a Sunday.";
+                } else if (dayOfTheWeek == 1) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[1] + " because you were born a female on a Monday.";
+                } else if (dayOfTheWeek == 2) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[2] + " because you were born a female on a Tuesday.";
+                } else if (dayOfTheWeek == 3) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[3] + " because you were born on a female a Wednesday.";
+                } else if (dayOfTheWeek == 4) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[4] + " because you were born a female on a Thursday.";
+                } else if (dayOfTheWeek == 5) {
+                    document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[5] + " because you were born a female on a Friday.";
+                } else {
+
+                } document.getElementById("output").innerHTML = "Hi " + fname + ", your Born name is " + female[6] + " because you were born a female on a Saturday.";
+            }
+        }
+    }
+
+}
+
+function gender() {
+    var gender = document.getElementById("gender");
+    var genderName = parseInt(gender.options[gender.selectedIndex].value);
+
+    return genderName;
+
+}
+
+function day() {
+    var day = document.getElementById("day").value;
+    // var value = parseInt(day.options[day.selectedIndex].value);
+
+    var date = parseInt(day);
+    if (day == "") {
+        var notANumber = 0;
+        return notANumber;
+    } else {
+        return date;
+    }
+
+}
+
+function month() {
+    var month = document.getElementById("month");
+    var monthValue = parseInt(month.options[month.selectedIndex].value);
+    return monthValue;
+}
+
+function year() {
+    var year = document.getElementById("year");
+    var birthYear = year.options[year.selectedIndex].text;
+    var Y = birthYear.slice(2, 4);
+    var YY = parseInt(Y);
+    var yearCode = (YY + (YY / 4)) % 7
+    return yearCode;
+}
+
+function century() {
+    var century = document.getElementById("year");
+    var birthCentury = century.options[century.selectedIndex].text;
+    var C = birthCentury.slice(0, 2);
+    var CC = parseInt(C);
+    if (CC == 19) {
+        var centuryCode = 0;
+        return centuryCode;
+    } else {
+        var cCentury = 6;
+        return cCentury;
+    }
+}
+
+function leapYearCode() {
+    var year = document.getElementById("year");
+    var birthYear = year.options[year.selectedIndex].text;
+    var Y = parseInt(birthYear);
+    if (Y % 4 == 0) {
+        var data = 1;
+        return data;
+    } else {
+        var notLeap = 0;
+        return notLeap;
+    }
+
+
+}
+
+function reset() {
+    location.reload();
+}
